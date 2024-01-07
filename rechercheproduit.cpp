93166bf7d46720a4c0b4e4b535455e76cd8c34b8
Nssaiba <131682630+Nssaiba@users.noreply.github.com>
2024-01-07 23:10:16 +0100

    Update rechercheproduit.cpp

--- rechercheproduit.cpp
+++ rechercheproduit.cpp
@@ -6,7 +6,8 @@
 #include <QSqlQueryModel>
 RechercheProduit::RechercheProduit(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::RechercheProduit)
+    ui(new Ui::RechercheProduit),
+    model(nullptr)
 {
     ui->setupUi(this);
 }
@@ -14,12 +15,12 @@ RechercheProduit::RechercheProduit(QWidget *parent) :
 RechercheProduit::~RechercheProduit()
 {
     delete ui;
+     delete model;
 }
 
 void RechercheProduit::on_btnRechercheProduit_clicked()
 {
     QString NomP = ui->txtRechercheProduit->text();
-
 
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("c:/Users/Dell/gestion_stock.db");
@@ -32,20 +33,24 @@ void RechercheProduit::on_btnRechercheProduit_clicked()
 
     QSqlQuery query(db);
 
-    // Use a parameterized query to prevent SQL injection
-    query.prepare("select * from Produit where Name like '%"+NomP+"%'");
-    query.exec();
-    qDebug() << query.lastError().text() << query.lastQuery();
-    if(model == NULL){
+    // Utilise une requête paramétrée pour éviter l'injection SQL
+    query.prepare("SELECT * FROM Produit WHERE Name LIKE :NomP");
+    query.bindValue(":NomP", "%" + NomP + "%");
 
-        model=new QSqlQueryModel();
+    if (!query.exec()) {
+        qDebug() << "Query execution error: " << query.lastError().text();
+        return;
     }
 
+    if (model == nullptr) {
+        model = new QSqlQueryModel();
+    }
 
     model->setQuery(query);
     ui->tableView->setModel(model);
-    ui->tableView->setColumnWidth(1,200);
-    ui->tableView->setColumnWidth(3,300);
+    ui->tableView->setColumnWidth(1, 200);
+    ui->tableView->setColumnWidth(3, 300);
     qDebug() << "rows are : " << model->rowCount();
     ui->tableView->show();
 }
+
